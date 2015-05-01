4a9eb358d3f0cb6fc17ae9aff95438ef09b6e63d
George <spamgoga@gmail.com>
2015-05-01 14:17:44 +0400

    conjugate gradients good

--- conjugate_gradients/conjugate_gradients/Program.cs
+++ conjugate_gradients/conjugate_gradients/Program.cs
@@ -30,7 +30,7 @@ namespace conjugate_gradients // метод сопряженных градие�
                 );
         }
 
-        public static vec2 operator +(vec2 v1)
+        public static vec2 operator -(vec2 v1)
         {
             return new vec2(
                     -v1.x,
@@ -106,18 +106,20 @@ namespace conjugate_gradients // метод сопряженных градие�
         }
                 
 
-        public double dichotomyMinLambda(Func<vec2, double> f, vec2 p, double[] range, double eps, double delta)
+        public double argmin_lambda(Func<vec2, double> f, vec2 p, vec2 s, double[] range, double eps)
         {
             double x1, x2;
 
+            double delta = eps / 10;
+
             double from = range[0], to = range[1];
 
-            while (Math.Abs(f_lambda(f, p, to) - f_lambda(f, p, from)) > eps && Math.Abs(to - from) > 4 * delta)
+            while (Math.Abs(f_lambda(f, p, s, to) - f_lambda(f, p, s, from)) > eps && Math.Abs(to - from) > 4 * delta)
             {
 
                 x1 = (to + from) / 2 - delta;
                 x2 = (to + from) / 2 + delta;
-                if (f_lambda(f, p, x1) >= f_lambda(f, p, x2))
+                if (f_lambda(f, p, s, x1) >= f_lambda(f, p, s, x2))
                 {
                     from = x1;
                 }
@@ -127,19 +129,16 @@ namespace conjugate_gradients // метод сопряженных градие�
                 }
 
                 var lmbd = (from + to) / 2;
-                LogBoth(String.Format("  lmbd={2} f={3}", from, to, lmbd, f_lambda(f, p, lmbd)));
+                LogBoth(String.Format("        lmbd={2} f={3}", from, to, lmbd, f_lambda(f, p, s, lmbd)));
             }
 
             return (from + to) / 2;
         }
 
-        public double f_lambda(Func<vec2, double> f, vec2 p, double lambda)
+        public double f_lambda(Func<vec2, double> f, vec2 p, vec2 s,  double lambda)
         {
             var gr = grad_n(f, p);
-            return f(new vec2(
-                    p.x - lambda * gr.x,
-                    p.y - lambda * gr.y
-                ));
+            return f(p + lambda*s);
         }
 
         public double n_vec(vec2 v)
@@ -149,10 +148,43 @@ namespace conjugate_gradients // метод сопряженных градие�
 
 
         public vec2 getMin(Func<vec2, double> f, vec2 startPoint, double eps) {
-            vec2 p = startPoint;
+            vec2 xk = startPoint;
 
+            var k = 0;
+            while (true) {
+                LogBoth(String.Format("k = {0}", k));
+                vec2 skj = -grad_n(f, xk);
 
-            return p;
+                vec2 xkj = xk;
+                var j = 0;
+                while (true) {
+                    LogBoth(String.Format("    j = {0}", j));
+                    double lambda = argmin_lambda(f, xkj, skj, new double[] { -1, 1 }, eps);
+                    vec2 xkj_next = xkj + lambda*skj;
+                    double w = Math.Pow(n_vec(grad_n(f, xkj_next)), 2) /
+                            Math.Pow(n_vec(grad_n(f, xkj)), 2);
+                    vec2 skj_next = -grad_n(f, xkj_next) + w * skj;
+
+                    LogBoth(String.Format("check conditions: ||Skj_next|| = {0}; |xkj_next - xkj| = {1}; eps = {2}",
+                                                n_vec(skj_next), n_vec(xkj_next - xkj),
+                                                eps));
+                    if (n_vec(skj_next) < eps || n_vec(xkj_next - xkj) < eps) {
+                        LogBoth(String.Format("FOUND MIN at {0}", xkj_next.ToString()));
+                        return xkj_next;
+                    }else{
+                        if (j + 1 < 2) {
+                            j++;
+                            continue;
+                        }else{
+                            LogBoth(String.Format("next k, at point {0}", xkj_next.ToString()));
+                            xk = xkj_next;
+                            break;
+                        }
+                    }
+                }
+
+                k++;
+            }
         }
 
         static void Main(string[] args)
@@ -160,10 +192,73 @@ namespace conjugate_gradients // метод сопряженных градие�
             txtMirror = new StreamWriter("output.txt");
             var inst = new Program();
 
-            
+            bool flag = true;
+            while (flag)
+            {
+                double start_x, start_y, eps;
+
+                LogBoth("Input start_x: ");
+                if (!double.TryParse(Console.ReadLine(), out start_x))
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+                else
+                {
+                    LogFile(start_x.ToString());
+                }
+
+                LogBoth("Input start_y: ");
+                if (!double.TryParse(Console.ReadLine(), out start_y))
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+                else
+                {
+                    LogFile(start_y.ToString());
+                }
 
 
-            Console.ReadKey();
+                LogBoth("Input eps: ");
+                if (!double.TryParse(Console.ReadLine(), out eps))
+                {
+                    Console.WriteLine("Wrong input");
+                    continue;
+                }
+                else
+                {
+                    LogFile(eps.ToString());
+                }
+
+                vec2 startPoint = new vec2(start_x, start_y);
+
+                LogBoth("START Himmelblau");
+                var hmmlbl_min = inst.getMin(f2, startPoint, eps);
+                LogBoth(String.Format("Found Himmelblau min: f({0}, {1}) = {2}", hmmlbl_min.x, hmmlbl_min.y, f2(hmmlbl_min)));
+
+                Console.WriteLine("Press any key to try with Rosenbrock. Esc - exit");
+                if (Console.ReadKey().KeyChar == 27)
+                {                    
+                    break;
+                }
+
+                LogBoth("START Rosenbrock");
+                var rsnbrck_min = inst.getMin(f1, startPoint, eps);
+                LogBoth(String.Format("Found Rosenbrock min: f({0}, {1}) = {2}", rsnbrck_min.x, rsnbrck_min.y, f1(rsnbrck_min)));
+
+                LogBoth("\n\n\nRESULTS:");
+                LogBoth(String.Format("Found Rosenbrock min: f({0}, {1}) = {2}", rsnbrck_min.x, rsnbrck_min.y, f1(rsnbrck_min)));
+                LogBoth(String.Format("Found Himmelblau min: f({0}, {1}) = {2}", hmmlbl_min.x, hmmlbl_min.y, f2(hmmlbl_min)));
+
+                txtMirror.Flush();
+
+                Console.WriteLine("File output created 'output.txt'.\nPress to try again. Esc - exit");
+                if (Console.ReadKey().KeyChar == 27)
+                {
+                    break;
+                }
+            }
         }
 
 
